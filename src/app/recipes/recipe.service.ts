febbcdc951ce8dd1791e081b37239bee226528ef
Lukasz <lys.lukasz555@gmail.com>
2019-07-28 21:09:57 +0200

    387/502 - more NgRx - NgRX completed

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,67 +1,67 @@
-import {Recipe} from './recipe.model';
-import {Injectable} from '@angular/core';
-import {Ingredient} from '../shared/ingredient.model';
-import {Subject} from 'rxjs';
-import {Store} from '@ngrx/store';
-import {AddIngredients} from '../shopping-list/store/shopping-list.actions';
-import * as fromApp from '../store/app.reducer'
-
-@Injectable()
-export class RecipeService {
-  recipesChanged = new Subject<Recipe[]>();
-  recipeSelected = new Subject<Recipe>();
-
-  // private recipes: Recipe[] = [
-  //   new Recipe('A Test Recipe',
-  //     'Test desc',
-  //     'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg',
-  //     [new Ingredient('Meat', 1),
-  //       new Ingredient('Fries', 20)
-  //     ]),
-  //   new Recipe('Another Test Recipe',
-  //     'Test desc',
-  //     'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg',
-  //     [
-  //       new Ingredient('Buns', 2),
-  //       new Ingredient('Meat', 1)
-  //     ])
-  // ];
-  private recipes: Recipe[] = [];
-
-  constructor(private store: Store<fromApp.AppState>) {
-  }
-
-  getRecipes() {
-    return this.recipes.slice();
-  }
-
-  addIngredientsToShoppingList(ingredients: Ingredient[]) {
-    this.store.dispatch(new AddIngredients(ingredients));
-  }
-
-  getRecipe(id: number): Recipe {
-    return this.recipes[id];
-  }
-
-  addRecipe(recipe: Recipe) {
-    this.recipes.push(recipe);
-    this.recipesChanged.next(this.recipes.slice());
-
-  }
-
-  updateRecipe(index: number, newRecipe: Recipe) {
-    this.recipes[index] = newRecipe;
-    this.recipesChanged.next(this.recipes.slice());
-
-  }
-
-  deleteRecipe(index: number) {
-    this.recipes.splice(index, 1);
-    this.recipesChanged.next(this.recipes.slice());
-  }
-
-  setRecipes(recipes: Recipe[]) {
-    this.recipes = recipes;
-    this.recipesChanged.next(this.recipes.slice());
-  }
-}
+// import {Recipe} from './recipe.model';
+// import {Injectable} from '@angular/core';
+// import {Ingredient} from '../shared/ingredient.model';
+// import {Subject} from 'rxjs';
+// import {Store} from '@ngrx/store';
+// import {AddIngredients} from '../shopping-list/store/shopping-list.actions';
+// import * as fromApp from '../store/app.reducer'
+//
+// @Injectable()
+// export class RecipeService {
+//   recipesChanged = new Subject<Recipe[]>();
+//   recipeSelected = new Subject<Recipe>();
+//
+//   // private recipes: Recipe[] = [
+//   //   new Recipe('A Test Recipe',
+//   //     'Test desc',
+//   //     'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg',
+//   //     [new Ingredient('Meat', 1),
+//   //       new Ingredient('Fries', 20)
+//   //     ]),
+//   //   new Recipe('Another Test Recipe',
+//   //     'Test desc',
+//   //     'https://static01.nyt.com/images/2015/08/14/dining/14ROASTEDSALMON/14ROASTEDSALMON-articleLarge.jpg',
+//   //     [
+//   //       new Ingredient('Buns', 2),
+//   //       new Ingredient('Meat', 1)
+//   //     ])
+//   // ];
+//   private recipes: Recipe[] = [];
+//
+//   constructor(private store: Store<fromApp.AppState>) {
+//   }
+//
+//   getRecipes() {
+//     return this.recipes.slice();
+//   }
+//
+//   addIngredientsToShoppingList(ingredients: Ingredient[]) {
+//     this.store.dispatch(new AddIngredients(ingredients));
+//   }
+//
+//   getRecipe(id: number): Recipe {
+//     return this.recipes[id];
+//   }
+//
+//   addRecipe(recipe: Recipe) {
+//     this.recipes.push(recipe);
+//     this.recipesChanged.next(this.recipes.slice());
+//
+//   }
+//
+//   updateRecipe(index: number, newRecipe: Recipe) {
+//     this.recipes[index] = newRecipe;
+//     this.recipesChanged.next(this.recipes.slice());
+//
+//   }
+//
+//   deleteRecipe(index: number) {
+//     this.recipes.splice(index, 1);
+//     this.recipesChanged.next(this.recipes.slice());
+//   }
+//
+//   setRecipes(recipes: Recipe[]) {
+//     this.recipes = recipes;
+//     this.recipesChanged.next(this.recipes.slice());
+//   }
+// }
--- src/app/recipes/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,24 +1,29 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Params, Router} from '@angular/router';
 import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
-import {RecipeService} from '../../recipe.service';
+import * as fromApp from '../../../store/app.reducer';
+import {Store} from '@ngrx/store';
+import {map} from 'rxjs/operators';
+import * as RecipeActions from '../../store/recipe.actions';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrls: ['./recipe-edit.component.css']
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy {
   id: number;
   editMode = false;
   recipeForm: FormGroup;
+  private storeSub: Subscription;
+
 
   get ingredientsControls() {
     return (this.recipeForm.get('ingredients') as FormArray).controls;
   }
 
-  constructor(private route: ActivatedRoute, private recipeService: RecipeService,
-              private router: Router) {
+  constructor(private route: ActivatedRoute, private router: Router, private store: Store<fromApp.AppState>) {
   }
 
   ngOnInit() {
@@ -31,29 +36,39 @@ export class RecipeEditComponent implements OnInit {
     );
   }
 
+  ngOnDestroy(): void {
+    if (this.storeSub) {
+      this.storeSub.unsubscribe();
+    }
+  }
+
   private initForm() {
     let recipeName = '';
     let recipeImagePath = '';
     let recipeDescription = '';
     let recipeIngredients = new FormArray([]);
     if (this.editMode) {
-      const recipe = this.recipeService.getRecipe(this.id);
-      recipeName = recipe.name;
-      recipeImagePath = recipe.imagePath;
-      recipeDescription = recipe.description;
-      if(recipe['ingredients']) {
-        for (let ingredient of recipe.ingredients) {
-          recipeIngredients.push(
-            new FormGroup({
-              'name': new FormControl(ingredient.name, Validators.required),
-              'amount': new FormControl(ingredient.amount, [
-                Validators.required,
-                Validators.pattern(/^[1-9]+[0-9]*$/)
-              ])
-            })
-          );
+      // const recipe = this.recipeService.getRecipe(this.id);
+      this.storeSub = this.store.select('recipes').pipe(map(recipeState => {
+        return recipeState.recipes.find((recipe, index) => index === this.id);
+      })).subscribe(recipe => {
+        recipeName = recipe.name;
+        recipeImagePath = recipe.imagePath;
+        recipeDescription = recipe.description;
+        if (recipe['ingredients']) {
+          for (let ingredient of recipe.ingredients) {
+            recipeIngredients.push(
+              new FormGroup({
+                'name': new FormControl(ingredient.name, Validators.required),
+                'amount': new FormControl(ingredient.amount, [
+                  Validators.required,
+                  Validators.pattern(/^[1-9]+[0-9]*$/)
+                ])
+              })
+            );
+          }
         }
-      }
+      });
     }
     this.recipeForm = new FormGroup({
       'name': new FormControl(recipeName, Validators.required),
@@ -64,16 +79,18 @@ export class RecipeEditComponent implements OnInit {
   }
 
   onSubmit() {
-    if(this.editMode){
+    if (this.editMode) {
       // const newRecipe = new Recipe(
       //   this.recipeForm.value['name'],
       //   this.recipeForm.value['description'],
       //   this.recipeForm.value['imagePAth'],
       //   this.recipeForm.value['ingredients']
       //   );
-      this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+      // this.recipeService.updateRecipe(this.id, this.recipeForm.value);
+      this.store.dispatch(new RecipeActions.UpdateRecipe({index: this.id, newRecipe: this.recipeForm.value}));
     } else {
-      this.recipeService.addRecipe(this.recipeForm.value);
+      // this.recipeService.addRecipe(this.recipeForm.value);
+      this.store.dispatch(new RecipeActions.AddRecipe(this.recipeForm.value));
     }
     this.onCancel();
   }
